d4371e99059a2474cf176a296ffe30033c3f0644
Keertan Gir <gkeertangir@gmail.com>
2026-03-08 02:06:07 +0500

    SubArrayWithKddan's Algorithm

---
+++ BestTimeToBuyAndSell/practicePrograms/MaxSubArray.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg75hard.BestTimeForBuyAndSell.Practice;
+
+/**
+ *
+ * @author hp
+ */
+public class MaxSubArray {
+    
+    public static int maxSubArr(int [] arr){
+       // Stores the result (maximum sum found so far)
+       int res = arr[0];
+         // Maximum sum of subarray ending at current position
+       int maxEnding = arr[0];
+//       O(n)
+        for(int i = 0;i<arr.length; i++){
+            
+            // Either extend the previous subarray or start 
+            // new from current element
+//             5                 5   +  1   6    ,   1
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+            
+            // Update result if the new subarray sum is larger
+            
+//            6            5    6
+            res = Math.max(res , maxEnding);
+        }
+        
+//               6
+        return res;
+    }
+    
+    public static void main(String [] args){
+    
+        int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        
+        int maxsub = maxSubArr(arr);
+        System.out.println(maxsub);
+        
+        
+        
+    }
+    
+}
